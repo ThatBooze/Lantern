9be3e299b271ea2c744393cfa4d0ddc4471b78c0
Booze.dev <XMbosse50@gmail.com>
2024-10-22 13:32:16 -0400

    More work woohoo!!
    end me.

--- Lantern/Cogs/tags.py
+++ Lantern/Cogs/tags.py
@@ -1,10 +1,29 @@
 import os
+import json
 import discord
 from discord.ext import commands
 
+with open('Lantern/Resources/configuration.json') as config:
+    data = json.load(config)
 
-def generate_list():
+
+def generate_choices():
     return [f[:-3] for f in os.listdir('Lantern/Resources/Tags') if f.endswith(".md")]
+
+
+async def parse_markdown(ctx, selection, target):  # TODO: Add support for Webhooks.
+    try:  # TODO: Simplify try statement?
+        with open(f"Lantern/Resources/Tags/{selection}.md", 'r', encoding='utf-8') as markdown:
+            await ctx.send_response(content=f"{target} | {markdown.read()}", ephemeral=True)
+
+    except FileNotFoundError as e:
+        print(e, flush=True)
+
+        embed = discord.Embed(
+            color=discord.Color.brand_red(),
+            description="Whoops!\nFile not found."
+        )
+        await ctx.send_response(embed=embed, ephemeral=True)
 
 
 class TAGS(commands.Cog):
@@ -13,9 +32,15 @@ class TAGS(commands.Cog):
 
     @commands.slash_command()
     async def tag(self, ctx: discord.ApplicationContext,
-                  selection: discord.Option(str, choices=generate_list()),
+                  selection: discord.Option(str, choices=generate_choices()),
                   target: discord.Member = None):
-        await ctx.respond(content=f"{selection} | {target}", ephemeral=(target is None or target == ctx.author))
+        await parse_markdown(ctx, selection, target)  # TODO: Write prefix command.
+        # await ctx.respond(content=f"{selection} | {target}", ephemeral=(target is None or target == ctx.author))
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.startswith(f"{data.get('PREFIX')}tag"):
+            await message.channel.send(f"{message.content[5:]}")
 
 
 def setup(bot):
